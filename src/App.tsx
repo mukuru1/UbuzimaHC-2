00fbc93c2c6a4f978d2b754a42b97c5d9a2e0648
mukuru1 <kabengaedwin@gmail.com>
2025-07-03 12:00:42 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,34 @@ import Settings from './components/settings/Settings';
 import MedicalRecords from './components/medical-records/MedicalRecords';
 import PatientDashboard from './components/dashboard/PatientDashboard';
 import EnvironmentCheck from './components/EnvironmentCheck';
+import { supabase } from './lib/supabase'
+import { useState, useEffect } from 'react'
+
+function Page() {
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    function getTodos() {
+      const { data: todos } = await supabase.from('todos').select()
+
+      if (todos.length > 1) {
+        setTodos(todos)
+      }
+    }
+
+    getTodos()
+  }, [])
+
+  return (
+    <div>
+      {todos.map((todo) => (
+        <li key={todo}>{todo}</li>
+      ))}
+    </div>
+  )
+}
+export default Page
+
 
 const AppContent: React.FC = () => {
   const { currentView } = useApp();
